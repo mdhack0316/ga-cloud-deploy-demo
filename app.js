b161dcb9cfaadcdf43253fc1aad9be40ef6c4da0
Barahalikar Siddharth <barahalikar.siddharth@gmail.com>
2024-03-06 15:27:44 +0530

    fdg

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000; 
   
 app.get('/', (req, res) => { 
-  res.send('Wow!!!!!'); 
+  res.send('Wow!!!!! '+ process.env.XYZ); 
 }); 
   
 app.listen(port, () => { 
